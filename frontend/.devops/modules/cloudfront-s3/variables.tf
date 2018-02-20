3546a41462d867283538db8961746095ac483375
David Gage <whytheplatypus@gmail.com>
2018-02-19 19:55:25 -0500

    Add initial terraform files for frontend Manage our route53, s3, and cloudfront resources. Allows us to easily add, remove, reconfigure, etc. our infrastructure.

---
+++ frontend/.devops/modules/cloudfront-s3/variables.tf
@@ -0,0 +1,44 @@
+variable region {
+  default = "us-east-1"
+}
+
+variable project {
+  default = "noproject"
+}
+variable environment {
+  default = "default"
+}
+
+variable domain {}
+
+variable bucket_name {
+  description = "The name of the S3 bucket to create."
+}
+
+variable duplicate-content-penalty-secret {}
+variable deployer {}
+variable acm-certificate-arn {}
+
+variable routing_rules {
+  default = ""
+}
+
+variable not-found-response-path {
+  default = "/404.html"
+}
+
+variable "tags" {
+  type        = "map"
+  description = "Optional Tags"
+  default     = {}
+}
+
+variable "trusted_signers" {
+  type = "list"
+  default = []
+}
+
+variable "forward-query-string" {
+  description = "Forward the query string to the origin"
+  default     = false
+}
