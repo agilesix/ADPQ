fba2c6103582af416bdadaeac3dda78e44ac2370
Aurora Hay <aurora.hay@agile6.com>
2018-02-19 19:29:45 -0800

    add rswag and example

--- backend/web/config/routes.rb
+++ backend/web/config/routes.rb
@@ -1,6 +1,8 @@
 require "sidekiq/web"
 
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   mount_devise_token_auth_for 'User', at: 'auth'
   mount Sidekiq::Web => '/sidekiq'
 
---
+++ backend/web/spec/integration/auth_spec.rb
@@ -0,0 +1,29 @@
+require 'swagger_helper'
+
+describe 'Authentication API' do
+
+  path '/auth' do
+    post 'create' do
+      tags 'User Registration'
+
+      consumes 'application/json'
+      produces 'application/json'
+
+      parameter name: :user_registration, in: :body, schema: {
+          type: :object,
+          properties: {
+              email: {type: :string},
+              password: {type: :string},
+              password_confirmation: {type: :string},
+              confirm_success_url: {type: :string},
+          }
+      }
+
+      response '200', 'user created' do
+        let(:blog) { { email: 'test@example.com', password: 'testexample', password_confirmation: 'testexample',  confirm_success_url: 'http://localhost:8080/'} }
+        run_test!
+      end
+
+    end
+  end
+end
