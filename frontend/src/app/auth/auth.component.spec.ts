e3478dd4deeba97f1372fd8bc6176745ca64e567
bhalt <bhalteman@fearsol.com>
2018-02-22 17:17:46 -0500

    fix auth component test

--- frontend/src/app/auth/auth.component.spec.ts
+++ frontend/src/app/auth/auth.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AuthComponent } from './login.component';
+import { AuthComponent } from './auth.component';
 
 describe('AuthComponent', () => {
   let component: AuthComponent;
