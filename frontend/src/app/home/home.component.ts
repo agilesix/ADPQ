8ee0bb0e46102a9844a2e81dcfa22d4456cecc01
bhalt <bhalteman@fearsol.com>
2018-02-27 13:48:37 -0500

    initial user/admin home ui

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
-import {AuthService} from '../services/auth.service';
-import {Angular2TokenService} from "angular2-token";
-import {Router, RouterLink} from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { Angular2TokenService } from "angular2-token";
+import { Router, RouterLink } from '@angular/router';
+import { StepService } from '../services/step.service';
 
 @Component({
   selector: 'app-home',
@@ -9,13 +10,22 @@ import {Router, RouterLink} from '@angular/router';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  constructor(private cdr: ChangeDetectorRef, public authService: AuthService) { 
+  constructor(private cdr: ChangeDetectorRef, public authTokenService:Angular2TokenService, public authService: AuthService, public stepService: StepService) { 
   }
 
   ngOnInit() {    
     this.cdr.detectChanges();
     this.authService.isLoggedIn();
     this.cdr.detectChanges();
+    this.stepService.getWorkflowSteps();
   }   
+
+  hasRole(roleName) {
+    let userHasRole = false;
+    if (this.authTokenService.currentUserData && this.authTokenService.currentUserData['roles'].length != 0) {
+      userHasRole = this.authTokenService.currentUserData['roles'].some(r => r.name == roleName);
+    }
+    return userHasRole;
+  }
 
 }
