e9f2f206720779f9484a1899a6467c9a4dfe8b2e
bhalt <bhalteman@fearsol.com>
2018-02-27 23:01:11 -0500

    ui for user landing, admin landing, portfolio workflow, submission review workflow

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ChangeDetectorRef } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { Angular2TokenService } from "angular2-token";
 import { Router, RouterLink } from '@angular/router';
 import { StepService } from '../services/step.service';
+import { ModalComponent } from '../modal/modal.component';
 
 @Component({
   selector: 'app-home',
@@ -11,6 +12,12 @@ import { StepService } from '../services/step.service';
 })
 export class HomeComponent implements OnInit {
   constructor(private cdr: ChangeDetectorRef, public authTokenService:Angular2TokenService, public authService: AuthService, public stepService: StepService) { 
+  }
+    
+  @ViewChild('modal') modal: ModalComponent;
+
+  presentModal(mode) {
+    this.modal.openModal(mode);
   }
 
   ngOnInit() {    
