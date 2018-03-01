2747d17f0c8ef4dbb204e45785d39bb80266fd57
David Gage <whytheplatypus@gmail.com>
2018-03-01 17:03:11 -0500

    Allow approval and rejection of files by admins - Filter listed files by approved or not - List unapproved files in modal on homepage - Allow admin to reject or approve files

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -3,6 +3,7 @@ import { AuthService } from '../services/auth.service';
 import { Angular2TokenService } from "angular2-token";
 import { Router, RouterLink } from '@angular/router';
 import { StepService } from '../services/step.service';
+import { ArticleService} from '../services/article.service';
 import { ModalComponent } from '../modal/modal.component';
 
 @Component({
@@ -11,13 +12,25 @@ import { ModalComponent } from '../modal/modal.component';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  constructor(private cdr: ChangeDetectorRef, public authTokenService:Angular2TokenService, public authService: AuthService, public stepService: StepService) { 
-  }
-    
+  public fileSubmissionCount;
+
+  constructor(private cdr: ChangeDetectorRef,
+    public authTokenService: Angular2TokenService,
+    public authService: AuthService,
+    public stepService: StepService,
+    public articleService: ArticleService) {}
+
   @ViewChild('modal') modal: ModalComponent;
 
   presentModal(mode) {
     this.modal.openModal(mode);
+  }
+
+  refreshFileSubmissions() {
+    this.articleService.getFileSubmissions().subscribe(data => {
+      this.modal.fileSubmissions = data.json();
+      this.fileSubmissionCount = this.modal.fileSubmissions.length;
+    })
   }
 
   ngOnInit() {    
@@ -25,9 +38,26 @@ export class HomeComponent implements OnInit {
     this.authService.isLoggedIn();
     this.cdr.detectChanges();
     this.stepService.getWorkflowSteps();
+    this.refreshFileSubmissions();
+    this.modal.fileActions.subscribe(evt => {
+      switch (evt.action) {
+        case 'approve': {
+          this.articleService.approveFileAttachment(evt).subscribe(() => {
+            this.refreshFileSubmissions()
+          });
+          break;
+        }
+        case 'reject': {
+          this.articleService.removeFileAttachment(evt).subscribe(() => {
+            this.refreshFileSubmissions()
+          });
+          break;
+        }
+      }
+    });
   }   
 
-  hasRole(roleName) {
+   hasRole(roleName) {
     let userHasRole = false;
     if (this.authTokenService.currentUserData && this.authTokenService.currentUserData['roles'].length != 0) {
       userHasRole = this.authTokenService.currentUserData['roles'].some(r => r.name == roleName);
