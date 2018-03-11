9d8b261473d6d9ee2584896eee7cc9c779877451
Aurora Hay <aurora.hay@agile6.com>
2018-03-11 11:06:51 -0700

    fix seeds file

--- backend/web/db/seeds.rb
+++ backend/web/db/seeds.rb
@@ -6,24 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-#Create Roles
-admin_role = Role.create!(name: 'Admin') unless Role.exists? name: 'Admin'
-contributor_role = Role.create!(name: 'Contributor') unless Role.exists? name: 'Contributor'
-
-#Create Users
-dan = User.create!(email: 'dan@a2tool.com', nickname: 'Dan', name: 'Dan', password: "dana2tool") unless User.find_by(email: 'dan@a2tool.com').present?
-chris = User.create!(email: 'chris@a2tool.com', nickname: 'Chris', name: 'Chris', password: "chrisa2tool") unless User.find_by(email: 'chris@a2tool.com').present?
-bob = User.create!(email: 'bob@a2tool.com', nickname: 'Bob', name: 'Bob', password: "boba2tool") unless User.find_by(email: 'bob@a2tool.com').present?
-tanya = User.create!(email: 'tanya@a2tool.com', nickname: 'Tanya', name: 'Tanya', password: "tanyaa2tool") unless User.find_by(email: 'tanya@a2tool.com').present?
-admin = User.create!(email: 'admin@a2tool.com', nickname: 'Admin', name: 'Admin', password: "admina2tool") unless User.find_by(email: 'admin@a2tool.com').present?
-
-#Assign Roles to Users
-admin.add_role 'Admin' unless admin.blank? || admin.has_role?('Admin')
-dan.add_role 'Admin' unless dan.blank? || dan.has_role?('Admin')
-chris.add_role 'Admin' unless chris.blank? || chris.has_role?('Admin')
-bob.add_role 'Contributor' unless bob.blank? || bob.has_role?('Contributor')
-tanya.add_role 'Contributor' unless tanya.blank? || tanya.has_role?('Contributor')
-
 #Create Workflow Type
 workflow_type = WorkflowType.create!(name: 'Acquisition Workflow Type', description: 'The workflow to acquire agile.') unless WorkflowType.exists? name: 'Acquisition Workflow Type'
 
@@ -50,6 +32,24 @@ wfs3 = WorkflowStep.create!(workflow: workflow, name: 'Concept of Operations (Co
 wfs4 = WorkflowStep.create!(workflow: workflow, name: 'Independent Government Cost Estimate (IGCE)', description: 'This is the description for the Independent Government Cost Estimate (IGCE) workflow step.') unless workflow.blank? || WorkflowStep.exists?(name: 'Independent Government Cost Estimate (IGCE)')
 wfs5 = WorkflowStep.create!(workflow: workflow, name: 'Proposal Instructions/Evaluation Criteria', description: 'This is the description for the Proposal Instructions/Evaluation Criteria workflow step.') unless workflow.blank? || WorkflowStep.exists?(name: 'Proposal Instructions/Evaluation Criteria')
 
+#Create Roles
+admin_role = Role.create!(name: 'Admin') unless Role.exists? name: 'Admin'
+contributor_role = Role.create!(name: 'Contributor') unless Role.exists? name: 'Contributor'
+
+#Create Users
+dan = User.create!(email: 'dan@a2tool.com', nickname: 'Dan', name: 'Dan', password: "dana2tool") unless User.find_by(email: 'dan@a2tool.com').present?
+chris = User.create!(email: 'chris@a2tool.com', nickname: 'Chris', name: 'Chris', password: "chrisa2tool") unless User.find_by(email: 'chris@a2tool.com').present?
+bob = User.create!(email: 'bob@a2tool.com', nickname: 'Bob', name: 'Bob', password: "boba2tool") unless User.find_by(email: 'bob@a2tool.com').present?
+tanya = User.create!(email: 'tanya@a2tool.com', nickname: 'Tanya', name: 'Tanya', password: "tanyaa2tool") unless User.find_by(email: 'tanya@a2tool.com').present?
+admin = User.create!(email: 'admin@a2tool.com', nickname: 'Admin', name: 'Admin', password: "admina2tool") unless User.find_by(email: 'admin@a2tool.com').present?
+
+#Assign Roles to Users
+admin.add_role 'Admin' unless admin.blank? || admin.has_role?('Admin')
+dan.add_role 'Admin' unless dan.blank? || dan.has_role?('Admin')
+chris.add_role 'Admin' unless chris.blank? || chris.has_role?('Admin')
+bob.add_role 'Contributor' unless bob.blank? || bob.has_role?('Contributor')
+tanya.add_role 'Contributor' unless tanya.blank? || tanya.has_role?('Contributor')
+
 #Create Knowledge Article(s)
 ka1 = KnowledgeArticle.create!(title: 'How to Create a Vision Statement', description: 'This is a short description of what the knowledge article is about.', body:'# This is a header', user: dan, published: true) unless dan.blank? || KnowledgeArticle.exists?(title: 'How to Create a Vision Statement')
 wfs_ka1 = WorkflowStepKnowledgeArticle.create!(workflow_step: wfs1, knowledge_article: ka1) unless (wfs1.blank? || ka1.blank?) || WorkflowStepKnowledgeArticle.exists?(workflow_step: wfs1, knowledge_article: ka1)
@@ -63,11 +63,3 @@ ft3 = FileType.create!(name: 'PDF') unless FileType.exists?(name: 'PDF')
 cat1 = Category.create!(name: 'Template') unless Category.exists?(name: 'Template')
 cat2 = Category.create!(name: 'Example') unless Category.exists?(name: 'Example')
 cat3 = Category.create!(name: 'Knowledge Reference') unless Category.exists?(name: 'Knowledge Reference')
-
-#start each user with a workflow package
-User.all.each do |u|
-  package = WorkflowPackage.create!(name: 'My First Solicitation Package', user: u, workflow: workflow) unless WorkflowPackage.find_by(user: u, workflow: workflow).present?
-  WorkflowStep.where(workflow: workflow).each do |wfs|
-    WorkflowStepPackage.create!(user: u, workflow_step: wfs, workflow_package: package) unless WorkflowStepPackage.find_by(user: u, workflow_step: wfs, workflow_package: package).present?
-  end
-end
